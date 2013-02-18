ad3f50f1388ce2c7426c52b0c8c9c23b39080632
jaymz <jaymz@u-dox.com>
2013-02-18 18:57:16 +0000

    few more problems sorted - seem to be missing some solutions to 11, 12

--- euler_helpers.py
+++ euler_helpers.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env/python2
-import math
 from collections import defaultdict
 
-from collections import defaultdict
 
 def is_prime(x):
-    """ 
-        Niavely checks if a given 'x' is prime 
+    """
+        Niavely checks if a given 'x' is prime
     """
     if x % 2 == 0:
         return False
@@ -15,19 +13,21 @@ def is_prime(x):
             return False
     return True
 
+
 def sieve(n):
     """
         Creates a list of primes up to and including n via sieve technique
     """
-    n = n+1
+    n = n + 1
     sieve = range(n)
     primes = defaultdict(bool)
 
     # Default our list to true - assume all are prime
-    for x in sieve: primes[x] = True
+    for x in sieve:
+        primes[x] = True
 
     p = 2
-    while p <= n and p**2 <= n:
+    while p <= n and p ** 2 <= n:
         for c, n in enumerate(sieve[p::p]):
             if c == 0 and primes[n] == True:
                 primes[n] = True
@@ -35,7 +35,8 @@ def sieve(n):
                 primes[n] = False
         p += 1
 
-    return [p for p in primes if primes[p]==True and p>1]
+    return [p for p in primes if primes[p] == True and p > 1]
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     pass
