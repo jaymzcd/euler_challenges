a5e05bb9d9e89ed918be966c73d39d1ec0758eda
jaymz <jaymz@u-dox.com>
2013-02-18 23:03:18 +0000

    problem 59 - xor encryption

--- euler.py
+++ euler.py
@@ -2,7 +2,7 @@
 #!/usr/bin/env python2
 import sys
 import math
-
+from collections import Counter
 from euler_helpers import *
 
 
@@ -647,6 +647,62 @@ def problem_26():
             x += 1
 
     print max_digit
+
+
+def problem_59():
+    """
+        Each character on a computer is assigned a unique code and the preferred
+        standard is ASCII (American Standard Code for Information Interchange). For
+        example, uppercase A = 65, asterisk (*) = 42, and lowercase k = 107.
+
+        A modern encryption method is to take a text file, convert the bytes to ASCII,
+        then XOR each byte with a given value, taken from a secret key. The advantage
+        with the XOR function is that using the same encryption key on the cipher
+        text, restores the plain text; for example, 65 XOR 42 = 107, then 107 XOR 42 =
+        65.
+
+        For unbreakable encryption, the key is the same length as the plain text
+        message, and the key is made up of random bytes. The user would keep the
+        encrypted message and the encryption key in different locations, and without
+        both "halves", it is impossible to decrypt the message.
+
+        Unfortunately, this method is impractical for most users, so the modified
+        method is to use a password as a key. If the password is shorter than the
+        message, which is likely, the key is repeated cyclically throughout the
+        message. The balance for this method is using a sufficiently long password key
+        for security, but short enough to be memorable.
+
+        Your task has been made easy, as the encryption key consists of three lower
+        case characters. Using cipher1.txt (right click and 'Save Link/Target As...'),
+        a file containing the encrypted ASCII codes, and the knowledge that the plain
+        text must contain common English words, decrypt the message and find the sum
+        of the ASCII values in the original text.
+    """
+
+    with open('cipher1.txt') as f:
+        text = [int(x) for x in f.read().split(',')]
+
+    triples = []
+    for x in range(0, len(text), 3):
+        triples.append(text[x:x + 3])
+    triples.pop()  # out by 1, so tidy up
+
+    cols = [[x[y] for x in triples] for y in range(3)]
+    counts = [Counter(col) for col in cols]
+
+    # Grab the highest counts using Counter.most_common(n) - returns the n
+    # counts, so take the first and the k from the (k, v) stored:
+    top_codes = [x.most_common(1)[0][0] for x in counts]
+
+    # Assuming the top character for each one is a space (asc 32) since
+    # the likely other top character is 'e' but we know this text is words
+    # so theres going to be a lot of spaces too:
+    key = [chr(c ^ 32) for c in top_codes]
+
+    def decrypt():
+        return [c ^ ord(key[cnt % 3]) for cnt, c in enumerate(text)]
+
+    print sum(decrypt())
 
 
 if __name__ == '__main__':
