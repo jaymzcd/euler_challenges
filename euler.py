e363bb2fc587b728d9eb2de4cfb0d3fcf2b3161e
jaymz <jaymz@u-dox.com>
2013-03-04 17:24:20 +0000

    problem 92, ineffiecently

--- euler.py
+++ euler.py
@@ -783,6 +783,45 @@ def problem_59():
     print sum(decrypt())
 
 
+def problem_92():
+    """
+        A number chain is created by continuously adding the square of the digits in a
+        number to form a new number until it has been seen before.
+
+        For example,
+
+        44 → 32 → 13 → 10 → 1 → 1
+        85 → 89 → 145 → 42 → 20 → 4 → 16 → 37 → 58 → 89
+
+        Therefore any chain that arrives at 1 or 89 will become stuck in an endless
+        loop. What is most amazing is that EVERY starting number will eventually
+        arrive at 1 or 89.
+
+        How many starting numbers below ten million will arrive at 89?
+    """
+
+    # Why am i doing this?! Because I want the 'easy as pi' achievement!
+    _next = lambda x:  sum([int(y) ** 2 for y in str(x)])
+
+    def chain(x):
+        """
+            Create a number chain for a given starting number x
+        """
+        v = _next(x)
+        while True:
+            v = _next(v)
+            if v in (1, 89):
+                # If either of the above numbers occur then we know we'll
+                # "fall" into that sequence so can stop
+                return v
+
+    # This eventually returns the correct answer but takes, afaic far too
+    # much time and should be quicker!
+    # 149816337 function calls in 255.543 seconds
+
+    print len([True for x in range(1, 10000001) if chain(x) == 89])
+
+
 if __name__ == '__main__':
     try:
         number = sys.argv[1]
